901e8a77b7f064466ddbeb3a875f5d0e599588b5
faber-smythe <johann.lefering@gmail.com>
2024-10-29 21:34:23 +0100

    online save

--- app/game/GameController.tsx
+++ app/game/GameController.tsx
@@ -1,36 +1,40 @@
-
 import * as BABYLON from "@babylonjs/core";
+import "@babylonjs/loaders";
 import { registerBuiltInLoaders } from "@babylonjs/loaders/dynamic";
 
 export default class GameController {
-  debug: boolean = true
-  loading: boolean = false
-  canvas: HTMLCanvasElement
-  engine: BABYLON.Engine
-  scene: BABYLON.Scene
+  debug: boolean = true;
+  loading: boolean = false;
+  canvas: HTMLCanvasElement;
+  engine?: BABYLON.Engine;
+  scene?: BABYLON.Scene;
   // baseMesh
   // pearls
 
   constructor(canvas: HTMLCanvasElement) {
-    this.loading = true
-    this.canvas = canvas
-    this.engine = new BABYLON.Engine(canvas)
-    this.scene = new BABYLON.Scene(this.engine)
-    if (this.debug) this.scene.debugLayer.show()
-    this.setUp()
+    this.loading = true;
+    this.canvas = canvas;
+    this.setUp();
+    registerBuiltInLoaders();
   }
 
   setUp = async () => {
-    // imports
-    registerBuiltInLoaders()
-    BABYLON.appendSceneAsync("./assets/Tetra.glb", this.scene).then(() => {
-      // Create a default arc rotate camera and light.
-      this.scene.createDefaultCameraOrLight(true, true, true);
+    await this.babylonInit();
+  };
 
-      this.engine.runRenderLoop(() => { this.scene.render() })
-    });
+  async babylonInit() {
+    // initialize
+    this.engine = new BABYLON.Engine(this.canvas);
+    this.scene = new BABYLON.Scene(this.engine);
+    if (this.debug) this.scene.debugLayer.show();
+    // import assets
+    const imported = BABYLON.SceneLoader.ImportMeshAsync(
+      "./",
+      "Tetra.glb",
+      undefined,
+      this.scene
+    );
+    console.log(await imported);
     // environment
   }
-
-
 }
